c6837b634b19450b6542cb6defb8826bccd8cb8f
Jan Barášek <janbarasek@gmail.com>
2019-10-27 16:50:49 +0100

    Polynomial: Add new type of token

---
+++ src/Entity/PolynomialToken.php
@@ -0,0 +1,93 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mathematicator\Tokenizer\Token;
+
+
+use Mathematicator\Engine\MathematicatorException;
+use Mathematicator\Numbers\NumberException;
+use Mathematicator\Numbers\SmartNumber;
+use Mathematicator\Tokenizer\Tokens;
+
+class PolynomialToken extends BaseToken
+{
+
+	/**
+	 * @var NumberToken
+	 */
+	private $times;
+
+	/**
+	 * @var NumberToken
+	 */
+	private $power;
+
+	/**
+	 * @var VariableToken
+	 */
+	private $variable;
+
+	/**
+	 * @var bool
+	 */
+	private $autoPower = false;
+
+	/**
+	 * @param NumberToken $times
+	 * @param NumberToken|int $power
+	 * @param VariableToken $variable
+	 * @throws NumberException|MathematicatorException
+	 */
+	public function __construct(NumberToken $times, ?NumberToken $power, VariableToken $variable)
+	{
+		if ($power === null) {
+			$this->autoPower = true;
+			$power = new NumberToken(new SmartNumber(0, '1'));
+			$power->setType(Tokens::M_NUMBER);
+			$power->setToken('1');
+			$power->setPosition($times->getPosition() + 1);
+		}
+
+		$this->times = $times;
+		$this->power = $power;
+		$this->variable = $variable;
+
+		$this->setToken($times->getToken() . '\cdot {' . $variable->getToken() . '}^{' . $power->getToken() . '}');
+		$this->setType(Tokens::M_POLYNOMIAL);
+		$this->setPosition($times->getPosition());
+	}
+
+	/**
+	 * @return NumberToken
+	 */
+	public function getTimes(): NumberToken
+	{
+		return $this->times;
+	}
+
+	/**
+	 * @return NumberToken
+	 */
+	public function getPower(): NumberToken
+	{
+		return $this->power;
+	}
+
+	/**
+	 * @return VariableToken
+	 */
+	public function getVariable(): VariableToken
+	{
+		return $this->variable;
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function isAutoPower(): bool
+	{
+		return $this->autoPower;
+	}
+
+}
